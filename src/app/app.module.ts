35d5bed19c31f59f0f8d4992c8a00d9dfe78e33d
Nibras M <nawaraaa2000@yahoo.com>
2020-08-26 07:12:27 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FunFactsComponent } from './fun-facts/fun-facts.component';
+import { PromisesComponent } from './promises/promises.component';
+import { CallbacksComponent } from './callbacks/callbacks.component';
+import { MapComponent } from './map/map.component';
+import { JavascriptComponent } from './javascript/javascript.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FunFactsComponent,
+    PromisesComponent,
+    CallbacksComponent,
+    MapComponent,
+    JavascriptComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
